fad87a09140fc1ebf89b4be1e6ed742eeaf36423
binodbit1 <70273346+binodbit1@users.noreply.github.com>
2023-02-11 14:58:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, request, redirect, render_template
 
 app = Flask(__name__)
 
-@app.route('/login', methods=['GET', 'POST'])
+@app.route('/', methods=['GET', 'POST'])
 def login():
     error = None
     if request.method == 'POST':
